f3dce69f4ed95617518d9203e4d57d3d37d4bef7
vtoshio <vtoshio2004@gmail.com>
2026-06-21 02:35:16 -0300

    style: padronização de tags e descrições de projetos

--- src/components/projects/AllProjectCard.vue
+++ src/components/projects/AllProjectCard.vue
@@ -3,7 +3,7 @@
 
 <template>
     <a href="https://www.github.com/vToshio" rel="noopener noreferrer" target="_blank"
-       class="group border border-dotted rounded-md transition-all duration-300 hover:shadow-lg hover:shadow-indigo-500/30 hover:cursor-pointer">
+       class="group border border-neutral-400/30 rounded-md transition-all duration-300 shadow hover:shadow-md hover:shadow-indigo-500/30 hover:cursor-pointer">
         <div class="flex justify-center items-center w-full h-full transition-color duration-300">
             <h2 class="text-2xl font-bold tracking-tight text-center p-10 group-hover:bg-linear-to-r group-hover:bg-clip-text group-hover:from-teal-500 group-hover:via-indigo-500 group-hover:to-purple-500 group-hover:text-transparent">
                 Veja todos os meus projetos clicando aqui!
